e4a6853e71eddbc10d6c0aba45a40bab228fb24e
Phuc Le <Bobgel12@gmail.com>
2019-11-13 23:22:52 -0800

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ const { User } = require('../models/User');
 var router = express.Router();
 
 router.put("/:userId", function (req, res) {
-	console.log(req.params.userId, req.body.user)
+	// console.log(req.params.userId, req.body.user)
     User.findByIdAndUpdate(req.params.userId, req.body.user, {new: true}, function (err, updatedUser) {
         if (!err) {
             res.status(200).send({ "err": null, item: updatedUser })
@@ -48,7 +48,7 @@ router.post("/conversation", function (req, res) {
     });
 })
 
-router.put("/conversation", function (req, res) {
+router.put("/conversation/update", function (req, res) {
 	const {conversationId, masterId, ninjaId, lastMessage } = req.body
 	let conObject = {
 		id: conversationId,
